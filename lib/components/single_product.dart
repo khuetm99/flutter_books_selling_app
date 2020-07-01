44f5b5fad8db503149864a8f14fdfdfdbaa9ab92
Khuetm <khueto1999@gmail.com>
2020-07-02 00:58:21 +0700

    fix gridview

--- lib/components/single_product.dart
+++ lib/components/single_product.dart
@@ -1,10 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'file:///D:/flutter_books_selling_app/lib/pages/product_detail.dart';
 import 'package:flutterbooksellingapp/models/products.dart';
 import 'package:flutterbooksellingapp/provider/product.dart';
 import 'package:provider/provider.dart';
-
-
 
 class Single_prod extends StatelessWidget {
   final product_ten;
@@ -94,6 +93,112 @@ class Single_prod extends StatelessWidget {
               width: 20,
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class Single_prod_Pages extends StatelessWidget {
+  final product_ten;
+  final product_tacgia;
+  final product_picture;
+  final product_giacu;
+  final product_gia;
+  final Product bookObject;
+
+  Single_prod_Pages(
+      {this.product_ten,
+      this.product_tacgia,
+      this.product_picture,
+      this.product_giacu,
+      this.product_gia,
+      this.bookObject});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      ProductDetails(bookObject: bookObject)));
+        },
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height: 220,
+                    width: 150,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0)),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5.0),
+                        child: Image.asset(
+                          product_picture,
+                          fit: BoxFit.fill,
+                        )),
+                  ),
+//                  SizedBox(
+//                    height: 15,
+//                  ),
+                  Expanded(
+                    child: Text(
+                      product_ten,
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black87,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+//                  SizedBox(
+//                    height: 7,
+//                  ),
+                  Expanded(
+                    child: Text(
+                      product_tacgia,
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.black45,
+                        fontWeight: FontWeight.w300,
+                      ),
+                      softWrap: true,
+                    ),
+                  ),
+//                  SizedBox(
+//                    height: 12,
+//                  ),
+                  Expanded(
+                    child: Text(product_gia + 'đ',
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500)),
+                  ),
+//                  SizedBox(
+//                    height: 12,
+//                  ),
+                  Expanded(
+                    child: Text(product_giacu + 'đ',
+                        style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.black,
+                            decoration: TextDecoration.lineThrough,
+                            fontWeight: FontWeight.w300)),
+                  ),
+                ],
+              ),
+//              SizedBox(
+//                width: 20,
+//              )
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/childrenbook.dart
+++ lib/pages/childrenbook.dart
@@ -36,6 +36,19 @@ List<Product> product_children_list = [
     nxb: 'Tuoi tre',
     description: 'a',
   ),
+  Product(
+    id: "12",
+    name: "Bố già",
+    author: "Mario puzo",
+    image: "images/products/p1.jpg",
+    old_price: '120,000',
+    price: '100,000',
+    nxb: 'Tuoi tre',
+    description:
+    ''' \n    Thế giới ngầm được phản ánh trong tiểu thuyết Bố Già là sự gặp gỡ giữa một bên là ý chí cương cường và nền tảng gia tộc chặt chẽ theo truyền thống mafia xứ Sicily với một bên là xã hội Mỹ nhập nhằng đen trắng, mảnh đất màu mỡ cho những cơ hội làm ăn bất chính hứa hẹn những món lợi kếch xù. Trong thế giới ấy, hình tượng Bố Già được tác giả dày công khắc họa đã trở thành bức chân dung bất hủ trong lòng người đọc. Từ một kẻ nhập cư tay trắng đến ông trùm tột đỉnh quyền uy, Don Vito Corleone là con rắn hổ mang thâm trầm, nguy hiểm khiến kẻ thù phải kiềng nể, e dè, nhưng cũng được bạn bè, thân quyến xem như một đấng toàn năng đầy nghĩa khí. Nhân vật trung tâm ấy đồng thời cũng là hiện thân của một pho triết lí rất “đời” được nhào nặn từ vốn sống của hàng chục năm lăn lộn giữa chốn giang hồ bao phen vào sinh ra tử, vì thế mà có ý kiến cho rằng “Bố Già là sự tổng hòa của mọi hiểu biết. Bố Già là đáp án cho mọi câu hỏi.
+
+   Với cấu tứ hoàn hảo, cốt truyện không thiếu những pha hành động gay cấn, tình tiết bất ngờ và không khí kình địch đến nghẹt thở, Bố Già xứng đáng là đỉnh cao trong sự nghiệp văn chương của Mario Puzo. Và như một cơ duyên đặc biệt, ngay từ năm 1971-1972, Bố Già đã đến với bạn đọc trong nước qua phong cách chuyển ngữ hào sảng, đậm chất giang hồ của dịch giả Ngọc Thứ Lang.''',
+  ),
 ];
 
 class ChildrenBook extends StatefulWidget {
@@ -51,18 +64,20 @@ class _ChildrenBookState extends State<ChildrenBook> {
       body: GridView.builder(
         padding: EdgeInsets.all(5),
         gridDelegate:
-            new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        physics: ScrollPhysics(),
+            new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,
+              childAspectRatio: MediaQuery.of(context).size.width /
+                (MediaQuery.of(context).size.height ),
+            ),
         itemCount: product_children_list.length,
         itemBuilder: (context, index) {
-          return Single_prod(
-            product_ten: product_children_list[index].name,
-            product_tacgia: product_children_list[index].author,
-            product_picture: product_children_list[index].image,
-            product_giacu: product_children_list[index].old_price,
-            product_gia: product_children_list[index].price,
-            bookObject: product_children_list[index],
-          );
+              return Single_prod_Pages(
+              product_ten: product_children_list[index].name,
+              product_tacgia: product_children_list[index].author,
+              product_picture: product_children_list[index].image,
+              product_giacu: product_children_list[index].old_price,
+              product_gia: product_children_list[index].price,
+              bookObject: product_children_list[index],
+            );
         },
       ),
     );
