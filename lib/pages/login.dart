9ccd3ad5b70c67eb7947f6fc5831783199c3977a
tuan4700 <thanleanhtuan4@gmail.com>
2020-07-05 23:10:13 +0700

    update carosel

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -28,106 +28,115 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       key: _key,
       backgroundColor: white,
-      body: authProvider.status == Status.Authenticating? Loading() : SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 100,
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset("images/logo.png", width: 120, height: 120,),
-              ],
-            ),
-
-            SizedBox(
-              height: 40,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: grey),
-                    borderRadius: BorderRadius.circular(15)
-                ),
-                child: Padding(padding: EdgeInsets.only(left: 10),
-                  child: TextFormField(
-                    controller: authProvider.email,
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Email",
-                        icon: Icon(Icons.email)
-                    ),
-                  ),),
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: grey),
-                    borderRadius: BorderRadius.circular(15)
-                ),
-                child: Padding(padding: EdgeInsets.only(left: 10),
-                  child: TextFormField(
-                    controller: authProvider.password,
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Password",
-                        icon: Icon(Icons.lock)
-                    ),
-                  ),),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: GestureDetector(
-                onTap: ()async{
-                  if(!await authProvider.signIn()){
-                    _key.currentState.showSnackBar(
-                        SnackBar(content: Text("Login failed!"))
-                    );
-                    return;
-                  }
-                  categoryProvider.loadCategories();
-                  productProvider.loadProducts();
-                  authProvider.clearController();
-                  changeScreenReplacement(context, HomePage());
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: red,
-                      border: Border.all(color: grey),
-                      borderRadius: BorderRadius.circular(15)
+      body: authProvider.status == Status.Authenticating
+          ? Loading()
+          : SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 100,
                   ),
-                  child: Padding(padding: EdgeInsets.only(top: 10, bottom: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.asset(
+                        "images/logo.png",
+                        width: 140,
+                        height: 140,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: grey),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: TextFormField(
+                          controller: authProvider.email,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Email",
+                              icon: Icon(Icons.email)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: grey),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: TextFormField(
+                          controller: authProvider.password,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Password",
+                              icon: Icon(Icons.lock)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: GestureDetector(
+                      onTap: () async {
+                        if (!await authProvider.signIn()) {
+                          _key.currentState.showSnackBar(
+                              SnackBar(content: Text("Login failed!")));
+                          return;
+                        }
+                        categoryProvider.loadCategories();
+                        productProvider.loadProducts();
+                        authProvider.clearController();
+                        changeScreenReplacement(context, HomePage());
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: red,
+                            border: Border.all(color: grey),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 10, bottom: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              CustomText(
+                                text: "Login",
+                                color: white,
+                                size: 22,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      changeScreen(context, RegistrationScreen());
+                    },
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        CustomText(text: "Login", color: white, size: 22,)
+                        CustomText(
+                          text: "Register here",
+                          size: 20,
+                        ),
                       ],
-                    ),),
-                ),
-              ),
-            ),
-
-            GestureDetector(
-              onTap: (){
-                changeScreen(context, RegistrationScreen());
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CustomText(text: "Register here", size: 20,),
+                    ),
+                  ),
                 ],
               ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
